7b99b20b1d6420f02c0c5cd822d498bd1edb6e3b
Jin <jinwyp@gmail.com>
2014-04-30 13:45:18 +0800

    add package

--- app/server/server.js
+++ app/server/server.js
@@ -30,7 +30,36 @@ if (Meteor.isServer) {
     Meteor.startup(function () {
 
         // code to run on server at startup
+
+
+
+/*        // bootstrap the admin user if they exist -- You'll be replacing the id later
+        if (Meteor.users.findOne("jkAjmuCsNzbm4qrMe"))
+            Roles.addUsersToRoles("jkAjmuCsNzbm4qrMe", ['admin']);
+
+        // create a couple of roles if they don't already exist (THESE ARE NOT NEEDED -- just for the demo)
+        if(!Meteor.roles.findOne({name: "normaluser"}))
+            Roles.createRole("normaluser");
+            */
+
+
     });
+
+
+/*
+
+    Accounts.onCreateUser(function(options, user) {
+
+        Roles.addUsersToRoles(user._id, ["normaluser"]);
+
+        var d6 = function () { return Math.floor(Random.fraction() * 6) + 1; };
+        user.dexterity = d6() + d6() + d6();
+
+        return user;
+    });
+*/
+
+
 
 
     Meteor.methods({
